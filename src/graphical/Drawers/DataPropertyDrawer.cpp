a1cd77b8b89c286128698336f319f27716dfd9c4
Guillaume Sarthou <gsarthou@laas.fr>
2018-10-04 09:58:07 +0200

    [FIX] prevent empty layers

--- src/graphical/Drawers/DataPropertyDrawer.cpp
+++ src/graphical/Drawers/DataPropertyDrawer.cpp
@@ -30,7 +30,9 @@ void DataPropertyDrawer::putInLayers()
       putLayer(layer);
       layer++;
     }
-    layer_nodes.pop_back();
+
+    if(layer_nodes.size() > 0)
+      layer_nodes.pop_back();
   }
 }
 
