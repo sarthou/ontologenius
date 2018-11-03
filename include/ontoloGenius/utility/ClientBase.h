06e319ac18cd66a5c49ed7ccd3fb57ebf243076b
Guillaume Sarthou <gsarthou@laas.fr>
2018-11-04 00:13:13 +0100

    [UPDATE] utility lib services restoration

--- include/ontoloGenius/utility/ClientBase.h
+++ include/ontoloGenius/utility/ClientBase.h
@@ -8,12 +8,26 @@
 #include <vector>
 #include <string>
 
+#ifndef COLOR_OFF
+#define COLOR_OFF     "\x1B[0m"
+#endif
+#ifndef COLOR_RED
+#define COLOR_RED     "\x1B[0;91m"
+#endif
+#ifndef COLOR_ORANGE
+#define COLOR_ORANGE  "\x1B[1;33m"
+#endif
+#ifndef COLOR_GREEN
+#define COLOR_GREEN   "\x1B[1;92m"
+#endif
+
 class ClientBase
 {
 public:
   ClientBase(ros::NodeHandle* n, std::string name) : client(n->serviceClient<ontologenius::OntologeniusService>("ontologenius/" + name, true))
   {
     n_ = n;
+    name_ = name;
   }
 
   size_t nb() {return cpt;}
@@ -35,10 +49,24 @@ protected:
     if(client.call(srv))
       return srv.response.values;
     else
-      return res;
+    {
+      std::cout << COLOR_ORANGE << "Failure to call ontoloGenius services" << COLOR_OFF << std::endl;
+      client = n_->serviceClient<ontologenius::OntologeniusService>("ontologenius/" + name_, true);
+      if(client.call(srv))
+      {
+        std::cout << COLOR_GREEN << "Restored ontoloGenius services" << COLOR_OFF << std::endl;
+        return srv.response.values;
+      }
+      else
+      {
+        std::cout << COLOR_RED << "Failure of service restoration" << COLOR_OFF << std::endl;
+        return res;
+      }
+    }
   }
 
 private:
+    std::string name_;
     ros::NodeHandle* n_;
     static size_t cpt;
 };
