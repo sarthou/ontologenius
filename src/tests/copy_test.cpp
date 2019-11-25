d4b88b331593e820f9d35a161d2d0b31996c91df
Guillaume Sarthou <gsarthou@laas.fr>
2019-11-25 16:07:20 +0100

    [ADD] deep copy CI

---
+++ src/tests/copy_test.cpp
@@ -0,0 +1,54 @@
+#include "ontoloGenius/core/ontoGraphs/Ontology.h"
+
+#include <chrono>
+#include <ros/package.h>
+
+#include <gtest/gtest.h>
+
+using namespace std::chrono;
+
+TEST(cpy_tests, reset)
+{
+  ontologenius::Ontology onto1;
+
+  std::string path_base = ros::package::getPath("ontologenius");
+  std::string path = path_base + "/files/attribute.owl";
+  onto1.readFromFile(path);
+  path = path_base + "/files/positionProperty.owl";
+  onto1.readFromFile(path);
+  path = path_base + "/files/testIndividuals.owl";
+  onto1.readFromFile(path);
+  onto1.close();
+
+  high_resolution_clock::time_point t1 = high_resolution_clock::now();
+  ontologenius::Ontology onto2 = onto1;
+  high_resolution_clock::time_point t2 = high_resolution_clock::now();
+  duration<double, std::milli> time_span = duration_cast<duration<double, std::milli>>(t2 - t1);
+  std::cout << "copy done in " << time_span.count() << " ms" << std::endl;
+
+  std::string indiv = "redCube";
+
+  EXPECT_TRUE(onto1.individual_graph_.getUp(indiv).size() == 11);
+  EXPECT_TRUE(onto2.individual_graph_.getUp(indiv).size() == 11);
+
+  std::string type = "cube";
+  onto2.individual_graph_.removeInheritage(indiv, type);
+  type = "object";
+  onto2.individual_graph_.addInheritage(indiv, type);
+
+  std::cout << "onto1 " << onto1.individual_graph_.getUp(indiv).size() << std::endl;
+  std::cout << "onto2 " << onto2.individual_graph_.getUp(indiv).size() << std::endl;
+
+  EXPECT_TRUE(onto1.individual_graph_.getUp(indiv).size() == 11);
+  EXPECT_TRUE(onto2.individual_graph_.getUp(indiv).size() == 7);
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "ontologenius_cpy");
+
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+
+  return 0;
+}
