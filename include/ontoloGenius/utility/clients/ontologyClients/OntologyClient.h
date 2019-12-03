a372d8ca0f8694ee011368674d5a027974bb9627
Guillaume Sarthou <gsarthou@laas.fr>
2019-12-03 06:41:04 +0100

    [FIX] ontologyClient API const argument to isA function

--- include/ontoloGenius/utility/clients/ontologyClients/OntologyClient.h
+++ include/ontoloGenius/utility/clients/ontologyClients/OntologyClient.h
@@ -11,7 +11,7 @@ public:
   }
 
   std::vector<std::string> getUp(const std::string& name, int depth = -1, const std::string& selector = "");
-  bool isA(std::string& name, const std::string& base_class);
+  bool isA(const std::string& name, const std::string& base_class);
   std::string getName(const std::string& name);
   std::vector<std::string> getNames(const std::string& name);
   std::vector<std::string> getEveryNames(const std::string& name);
