5a611847cd021313d67608a1e0eb8b675e553c1e
Julien Combattelli <julien.combattelli@hotmail.com>
2020-02-15 09:31:47 +0000

    Add support for clang-tidy

---
+++ cmake/ClangTidy.cmake
@@ -0,0 +1,15 @@
+find_program(
+    CLANG_TIDY_EXE
+    NAMES clang-tidy clang-tidy-9 clang-tidy-8 clang-tidy-7 clang-tidy-6
+    DOC "Path to clang-tidy executable"
+)
+
+if(CLANG_TIDY_EXE)
+    set(CMAKE_CXX_CLANG_TIDY
+        ${CLANG_TIDY_EXE}
+        --checks=*,-abseil*,-android*,-fuchsia*,-modernize-use-trailing-return-type*,-zircon*,-objc*,-mpi*,-openmp*
+	#,-google-readability*,-readability*,-hicpp-braces*
+    )
+else()
+    message(WARNING "No executable for clang-tidy found. Building without clang-tidy.")
+endif()
