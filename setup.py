e4e1b46a21ce86ea760eecc0b17169ad83bc5a77
feramozur <famozur@upo.es>
2021-03-25 17:10:20 +0100

    Fix CMake package installation logic

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['ontologenius'],
     package_dir={'': 'ontopy'},
-    requires=['rospy ontologenius ontologenius.srv']
+    requires=['rospy','ontologenius','ontologenius.srv']
 )
 
 setup(**d)
