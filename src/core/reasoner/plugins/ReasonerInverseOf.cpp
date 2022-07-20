b823ee98a73ba7281aa4b666f972fac27a589084
sarthou <guillaum324bis@hotmail.fr>
2022-07-20 11:01:22 +0200

    [ReasonerInverseOf] mark inverse individual as updated

--- src/core/reasoner/plugins/ReasonerInverseOf.cpp
+++ src/core/reasoner/plugins/ReasonerInverseOf.cpp
@@ -48,6 +48,7 @@ void ReasonerInverseOf::insertInverse(IndividualBranch_t* indiv_on, ObjectProper
     if(index == (int)indiv_on->object_relations_.size() - 1)
       indiv_on->object_properties_has_induced_.emplace_back();
     indiv_on->nb_updates_++;
+    inv_indiv->nb_updates_++;
 
     explanations_.emplace_back("[ADD]" + indiv_on->value() + "|" + inv_prop->value() + "|" + inv_indiv->value(),
                               "[ADD]" + inv_indiv->value() + "|" + base_prop->value() + "|" + indiv_on->value());
