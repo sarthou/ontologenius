9aa5c49529d817f949cde0654c9248a38f5859ad
Guillaume Sarthou <gsarthou@laas.fr>
2023-07-21 14:18:28 +0200

    [RosInterface] dataPropertyIndexHandle implementation

--- src/interface/RosInterfaceIndexHandlers.cpp
+++ src/interface/RosInterfaceIndexHandlers.cpp
@@ -1,4 +1,5 @@
 #include "ontologenius/interface/RosInterface.h"
+#include "ontologenius/interface/InterfaceParams.h"
 
 #include "ontologenius/core/utility/error_code.h"
 #include "ontologenius/graphical/Display.h"
@@ -15,7 +16,8 @@ bool RosInterface::classIndexHandle(ontologenius::OntologeniusIndexService::Requ
   else
   {
     removeUselessSpace(req.action);
-    param_t params = getParams(req.param);
+    InterfaceParams params;
+    params.extractIndexParams(req.param);
 
     reasoner_mutex_.lock();
     reasoners_.runPreReasoners(query_origin_class, req.action, params());
@@ -112,7 +114,8 @@ bool RosInterface::objectPropertyIndexHandle(ontologenius::OntologeniusIndexServ
   else
   {
     removeUselessSpace(req.action);
-    param_t params = getParams(req.param);
+    InterfaceParams params;
+    params.extractIndexParams(req.param);
 
     reasoner_mutex_.lock();
     reasoners_.runPreReasoners(query_origin_object_property, req.action, params());
@@ -191,67 +194,68 @@ bool RosInterface::dataPropertyIndexHandle(ontologenius::OntologeniusIndexServic
   else
   {
     removeUselessSpace(req.action);
-    param_t params = getParams(req.param);
+    InterfaceParams params;
+    params.extractIndexParams(req.param);
 
     reasoner_mutex_.lock();
     reasoners_.runPreReasoners(query_origin_data_property, req.action, params());
     reasoner_mutex_.unlock();
 
-    /*std::unordered_set<std::string> set_res;
+    std::unordered_set<index_t> set_res_index;
 
     if(req.action == "getDown")
-      set_res = onto_->data_property_graph_.getDown(params(), params.depth);
+      set_res_index = onto_->data_property_graph_.getDown(params.main_index, params.depth);
     else if(req.action == "getUp")
-      set_res = onto_->data_property_graph_.getUp(params(), params.depth);
+      set_res_index = onto_->data_property_graph_.getUp(params.main_index, params.depth);
     else if(req.action == "getDisjoint")
-      set_res = onto_->data_property_graph_.getDisjoint(params());
+      set_res_index = onto_->data_property_graph_.getDisjoint(params.main_index);
     else if(req.action == "getDomain")
-      set_res = onto_->data_property_graph_.getDomain(params());
+      set_res_index = onto_->data_property_graph_.getDomain(params.main_index);
     else if(req.action == "getRange")
-      set2vector(onto_->data_property_graph_.getRange(params()), res.values);
+      set2vector(onto_->data_property_graph_.getRange(params.main_index), res.index_values);
     else if(req.action == "getName")
     {
-      auto tmp = onto_->data_property_graph_.getName(params(), params.take_id);
+      auto tmp = onto_->data_property_graph_.getName(params.main_index, params.take_id);
       if(tmp != "")
-        res.values.push_back(tmp);
+        res.string_values.push_back(tmp);
     }
     else if(req.action == "getNames")
-      res.values = onto_->data_property_graph_.getNames(params(), params.take_id);
+      res.string_values = onto_->data_property_graph_.getNames(params.main_index, params.take_id);
     else if(req.action == "getEveryNames")
-      res.values = onto_->data_property_graph_.getEveryNames(params(), params.take_id);
+      res.string_values = onto_->data_property_graph_.getEveryNames(params.main_index, params.take_id);
     else if(req.action == "find")
-      set2vector(onto_->data_property_graph_.find<std::string>(params(), params.take_id), res.values);
+      set2vector(onto_->data_property_graph_.find<index_t>(params(), params.take_id), res.index_values);
     else if(req.action == "findSub")
-      set2vector(onto_->data_property_graph_.findSub<std::string>(params(), params.take_id), res.values);
+      set2vector(onto_->data_property_graph_.findSub<index_t>(params(), params.take_id), res.index_values);
     else if(req.action == "findRegex")
-      set2vector(onto_->data_property_graph_.findRegex<std::string>(params(), params.take_id), res.values);
+      set2vector(onto_->data_property_graph_.findRegex<index_t>(params(), params.take_id), res.index_values);
     else if(req.action == "findFuzzy")
     {
       if(params.threshold != -1)
-        set2vector(onto_->data_property_graph_.findFuzzy(params(), params.take_id, params.threshold), res.values);
+        set2vector(onto_->data_property_graph_.findFuzzy(params(), params.take_id, params.threshold), res.string_values);
       else
-        set2vector(onto_->data_property_graph_.findFuzzy(params(), params.take_id), res.values);
+        set2vector(onto_->data_property_graph_.findFuzzy(params(), params.take_id), res.string_values);
     }
     else if(req.action == "exist")
     {
-      if(onto_->data_property_graph_.touch(params()))
-        res.values.push_back(params());
+      if(onto_->data_property_graph_.touch(params.main_index))
+        res.index_values.push_back(params.main_index);
     }
     else if(req.action == "getAll")
-      res.values = onto_->data_property_graph_.getAll();
+      res.index_values = onto_->data_property_graph_.getAllIndex();
     else
       res.code = UNKNOW_ACTION;
 
-    if(params.selector != "")
+    if(params.selector_index != 0)
     {
       if((req.action == "getUp") || (req.action == "getDown") || (req.action == "getDisjoint"))
-        set_res = onto_->data_property_graph_.select(set_res, params.selector);
+        set_res_index = onto_->data_property_graph_.select(set_res_index, params.selector_index);
       else if(req.action == "getDomain")
-        set_res = onto_->class_graph_.select(set_res, params.selector);
+        set_res_index = onto_->class_graph_.select(set_res_index, params.selector_index);
     }
 
-    if(res.values.size() == 0)
-      set2vector(set_res, res.values);*/
+    if(res.index_values.size() == 0)
+      set2vector(set_res_index, res.index_values);
   }
 
   return true;
@@ -267,7 +271,8 @@ bool RosInterface::individualIndexHandle(ontologenius::OntologeniusIndexService:
   else
   {
     removeUselessSpace(req.action);
-    param_t params = getParams(req.param);
+    InterfaceParams params;
+    params.extractIndexParams(req.param);
 
     reasoner_mutex_.lock();
     reasoners_.runPreReasoners(query_origin_individual, req.action, params());
