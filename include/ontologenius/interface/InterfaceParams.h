1337e3481ae61320ab8f10d0e2f7ae759e3e3926
Guillaume Sarthou <gsarthou@laas.fr>
2023-07-21 14:17:51 +0200

    [InterfaceParams] extractIndexParams

--- include/ontologenius/interface/InterfaceParams.h
+++ include/ontologenius/interface/InterfaceParams.h
@@ -76,6 +76,60 @@ public:
         base += " " + str_params[i];
     }
   }
+
+  void extractIndexParams(const std::string& param)
+  {
+    std::vector<std::string> str_params = split(param, " ");
+
+    if(str_params.size())
+      base = str_params[0];
+
+    if(sscanf(str_params[0].c_str(), "%ld:%ld", &main_index, &optional_index) < 1)
+        main_index = 0;
+
+    for(size_t i = 1; i < str_params.size(); i++)
+    {
+      if((str_params[i] == "-d") || (str_params[i] == "--depth"))
+      {
+        i++;
+        int tmp = -1;
+        if(sscanf(str_params[i].c_str(), "%d", &tmp) != 1)
+          tmp = -1;
+        depth = tmp;
+      }
+      else if((str_params[i] == "-s") || (str_params[i] == "--selector"))
+      {
+        i++;
+        selector = str_params[i];
+        if(sscanf(str_params[i].c_str(), "%ld", &selector_index) != 1)
+          selector_index = 0;
+      }
+      else if((str_params[i] == "-t") || (str_params[i] == "--threshold"))
+      {
+        i++;
+        float tmp = -1;
+        if(sscanf(str_params[i].c_str(), "%f", &tmp) != 1)
+          tmp = -1;
+        threshold = tmp;
+      }
+      else if((str_params[i] == "-i") || (str_params[i] == "--take_id"))
+      {
+        i++;
+        bool tmp = false;
+        if(str_params[i] == "true")
+          tmp = true;
+
+        take_id = tmp;
+      }
+      else if(str_params[i].size() && str_params[i][0] == '-')
+      {
+        Display::warning("[WARNING] unknow parameter \"" + str_params[i] + "\"");
+        i++;
+      }
+      else if(str_params[i].size())
+        base += " " + str_params[i];
+    }
+  }
 };
 
 } // namespace ontologenius
