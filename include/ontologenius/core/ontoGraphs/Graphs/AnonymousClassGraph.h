ee36541b63642a060286f0cdbf0f141e521ff06c
BastienDssrd <bastien.dussard@gmail.com>
2023-11-14 13:28:32 +0100

    deletions

--- include/ontologenius/core/ontoGraphs/Graphs/AnonymousClassGraph.h
+++ include/ontologenius/core/ontoGraphs/Graphs/AnonymousClassGraph.h
@@ -6,7 +6,6 @@
 
 namespace ontologenius {
 
-
 struct Cardinality_t
 {
   std::string cardinality_type;
@@ -16,7 +15,6 @@ struct Cardinality_t
   std::string toString()
   {
     std::string res = " ";
-    //std::cout << "type" << cardinality_type + " number " + cardinality_number + " range " + cardinality_range;
     if(cardinality_type != "")
       res += cardinality_type + " ";
     if(cardinality_number != "")
@@ -30,7 +28,6 @@ struct Cardinality_t
   {
     return {cardinality_range, cardinality_number, cardinality_type};
   }
-  
 };
 
 struct Restriction_t
@@ -57,7 +54,6 @@ struct Restriction_t
   std::vector<std::string> toVector()
   {
     std::vector<std::string> result;
-    //card->cardinality_number.empty();
     if(property != "")
       result.push_back(property);
     if(card.cardinality_type != "")
@@ -69,7 +65,6 @@ struct Restriction_t
     if(restriction_range != "")
       result.push_back(restriction_range);
 
-    //std::cout << "property : " << property + " " + "cardinality : " << card->getCardinality() + " " + "restriction : " << restriction_range << std::endl;
     return result;
   }
 };
@@ -82,7 +77,6 @@ struct ExpressionMember_t
   bool is_data_property;
 
   Restriction_t rest; // Restriction (e.g hasComponent some Camera)
-  std::string class_restriction; // if the restriction is only a class restriction (e.g A Eq to B)
   std::vector<ExpressionMember_t*> intersects; // sub elements
   ExpressionMember_t* mother;
   std::string str_equivalence;
@@ -119,184 +113,6 @@ struct ExpressionMember_t
     
     if(logical_type_ == logical_not)
       str_equivalence += ")";
-
-  }
-
-// Distribution function with strings
-  void DistributeEquiv(){
-
-    // R1 or (R2 or R3) => 3 equivalences R1 / R2 / R3
-    // R1 and (R2 or R3) => 2 equivalences R1 and R2 / R1 and R3
-    // R1 or (R2 and R3) => 2 equivalences R1 / R2 and R3
-    // R1 and (R2 and R3) => 1 equivalence R1 and R2 and R3
-
-    bool distrib = false;
-    std::vector<std::vector<std::string>> list_or;
-    std::vector<std::string> list_2;
-
-    for(auto elem : intersects)
-    { // OR node
-      if(elem->andor == false && elem->nb_sub > 0)
-      {
-        std::vector<std::string> list_1;
-        //std::cout << "Distributable node" << elem->str_equivalence << std::endl;
-        distrib = true;
-        for(auto elem2 : elem->intersects)
-        {
-          list_1.push_back(elem2->str_equivalence);
-        }
-        list_or.push_back(list_1);
-      }
-      // restriction node
-      else if(elem->andor==false && elem->nb_sub == 0)
-      {
-        //std::cout << "Restriction " << elem->str_equivalence << std::endl;
-        list_2.push_back(elem->str_equivalence);
-      }
-    }
-
-    if(distrib)
-    {
-      std::cout << "Distributed expressions : " << std::endl;
-       // many to many
-      if(list_or.size() == 2)
-      {
-        std::cout << "two complex members" << std::endl;
-        std::vector<std::string> list_complex_left = list_or[0];
-        std::vector<std::string> list_complex_right = list_or[1];
-
-        for( auto& value1 : list_complex_left)
-        {
-          for( auto& value2 : list_complex_right)
-          {
-            std::cout << value1 + " and " + value2 << "\n";
-            distributed_equivalence += "(" + value1 + " and " + value2 +  ")";
-            if(&value2 != &list_complex_right.back())
-            {
-              distributed_equivalence += " or ";
-            }
-          }
-          if(&value1 != &list_complex_left.back())
-          {
-              distributed_equivalence += " or ";
-            }
-        }
-        std::cout <<" NEW equivalence is : " <<  distributed_equivalence << std::endl;
-      }
-      // one to many
-      else
-      {
-        std::cout << "one complex member" << std::endl;
-        std::vector<std::string> list_complex_left = list_or[0];
-        for( auto& value1: list_complex_left)
-        {
-          for( auto& value2: list_2)
-          {
-            std::cout << value1 + " and " + value2 << "\n";
-            distributed_equivalence += "(" + value1 + " and " + value2 +  ")";
-            if (&value1 != &list_complex_left.back()){
-              distributed_equivalence += " or ";
-            }
-          }
-        }
-        std::cout <<" NEW equivalence is : " <<  distributed_equivalence << std::endl;
-      }
-    }
-  }
-
-// Distribution function with expression members and transformation of the tree
-  void DistributeEquiv_v2(){
-
-    // R1 or (R2 or R3) => 3 equivalences R1 / R2 / R3
-    // R1 and (R2 or R3) => 2 equivalences R1 and R2 / R1 and R3
-    // R1 or (R2 and R3) => 2 equivalences R1 / R2 and R3
-    // R1 and (R2 and R3) => 1 equivalence R1 and R2 and R3
-
-    bool distrib = false;
-    std::vector<std::vector<ExpressionMember_t*>> list_children;
-    std::vector<ExpressionMember_t*> list_restrictions;
-    
-    for(auto elem : intersects)
-    { // OR node
-      if(elem->andor == false && elem->nb_sub > 0)
-      {
-        std::vector<ExpressionMember_t*> list_1;
-        //std::cout << "Distributable node" << elem->str_equivalence << std::endl;
-        distrib = true;
-        for(auto elem2 : elem->intersects)
-        {
-          list_1.push_back(elem2);
-        }
-        list_children.push_back(list_1);
-      }
-      // restriction node
-      else if(elem->andor==false && elem->nb_sub == 0)
-      {
-        //std::cout << "Restriction " << elem->str_equivalence << std::endl;
-        list_restrictions.push_back(elem);
-      }
-    }
-
-    if(distrib)
-    {
-      // erase the content of the And node intersections and transform it into an or node
-      this->intersects.clear();
-      this->andor=false;
-      
-      // std::cout << "Distributed expressions : " << std::endl;
-      // many to many
-
-      if(list_children.size() == 2)
-      {
-        //std::cout << "two complex members" << std::endl;
-        std::vector<ExpressionMember_t*> list_complex_left = list_children[0];
-        std::vector<ExpressionMember_t*> list_complex_right = list_children[1];
-
-        for( auto& value1 : list_complex_left)
-        {
-
-          for(auto& value2 : list_complex_right)
-          {
-            // creation of an AND node
-            ExpressionMember_t* exp2 = new ExpressionMember_t;
-            exp2->mother = this;
-            exp2->andor = true;
-            this->intersects.push_back(exp2);
-            // push back the sub elements of the newly created and node
-            value1->mother=exp2;
-            value2->mother=exp2;
-          
-            exp2->intersects.push_back(value1);
-            exp2->intersects.push_back(value2);
-            exp2->UpdateEquiv();
-          }
-        }
-      }
-      // one to many
-      else
-      {
-        //std::cout << "one complex member" << std::endl;
-        std::vector<ExpressionMember_t*> list_complex_left = list_children[0];
-        
-        for( auto& value1: list_complex_left)
-        { 
-          ExpressionMember_t* exp2 = new ExpressionMember_t;
-          exp2->mother = this;
-          exp2->andor = true;
-          this->intersects.push_back(exp2);
-          for( auto& value2: list_restrictions)
-          {
-            value1->mother=exp2;
-            value2->mother=exp2;
-
-            exp2->intersects.push_back(value1);
-            exp2->intersects.push_back(value2);
-            exp2->UpdateEquiv();
-          }
-        }
-      }
-      this->UpdateEquiv();
-    }
   }
 };
 
@@ -306,11 +122,9 @@ struct AnonymousClassVectors_t
   ExpressionMember_t* equivalence_tree;
   std::string str_equivalences;
   std::vector<std::vector<std::string>> equiv_vect;
-
 };
 
 //for friend
-
 class ObjectPropertyGraph;
 class DataPropertyGraph;
 class IndividualGraph;
@@ -322,7 +136,6 @@ class AnonymousClassChecker;
 // class AnonymousClassGraph : public OntoGraph<AnonymousClassBranch_t>
 class AnonymousClassGraph : public Graph<AnonymousClassBranch_t>
 {
-
   friend ObjectPropertyGraph;
   friend DataPropertyGraph;
   friend IndividualGraph;
@@ -357,7 +170,6 @@ private:
     IndividualGraph* individual_graph_;
 
     std::vector<AnonymousClassBranch_t*> anonymous_classes_;
-
 };
 
 } // namespace ontologenius
