08b189b8adfba8ce0d686ba78862822de680c588
BastienDssrd <bastien.dussard@gmail.com>
2023-12-07 18:52:06 +0100

    [Triplets] adding the Triplets and Triplet_t

--- include/ontologenius/core/ontoGraphs/Branchs/Triplet.h
+++ include/ontologenius/core/ontoGraphs/Branchs/Triplet.h
@@ -6,48 +6,48 @@ namespace ontologenius {
 template<typename S, typename P, typename O>
 struct Triplet_t
 {
-    Triplet_t(S* s, P* p, O* o) : subject(s), predicate(p), object(o) {}
-    S* subject;
-    P* predicate;
-    O* object;
+	Triplet_t(S* s, P* p, O* o) : subject(s), predicate(p), object(o) {}
+	S* subject;
+	P* predicate;
+	O* object;
 
-    bool operator==(const Triplet_t& other)
-    {
-        return ((subject == other.subject) &&
-                (predicate == other.predicate) &&
-                (object == other.object));
-    }
+	bool operator==(const Triplet_t& other)
+	{
+		return ((subject == other.subject) &&
+						(predicate == other.predicate) &&
+						(object == other.object));
+	}
 
-    bool equals(S* s, P* p, O* o)
-    {
-        return ((subject == s) &&
-                (predicate == p) &&
-                (object == o));
-    }
+	bool equals(S* s, P* p, O* o)
+	{
+		return ((subject == s) &&
+						(predicate == p) &&
+						(object == o));
+	}
 };
 
 template<typename S, typename P, typename O>
 class Triplets
 {
 public:
-    void push(S* subject,
-              P* predicate,
-              O* object)
-    {
-        triplets.emplace_back(subject, predicate, object);
-    }
+	void push(S* subject,
+						P* predicate,
+						O* object)
+	{
+		triplets.emplace_back(subject, predicate, object);
+	}
 
-    bool exist(S* subject,
-               P* predicate,
-               O* object)
-    {
-        return(std::find_if(triplets.begin(), triplets.end(),
-                            [subject, predicate, object](auto& triplet)
-                            {return triplet.equals(subject, predicate, object);})
-                            != triplets.end());
-    }
+	bool exist(S* subject,
+						P* predicate,
+						O* object)
+	{
+		return(std::find_if(triplets.begin(), triplets.end(),
+												[subject, predicate, object](auto& triplet)
+												{return triplet.equals(subject, predicate, object);})
+												!= triplets.end());
+	}
 
-    std::vector<Triplet_t<S,P,O>> triplets;
+	std::vector<Triplet_t<S,P,O>> triplets;
 };
 
 } // namespace ontologenius
