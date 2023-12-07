c07376b83a32ff2e6887ee031c30800ad29b12a7
BastienDssrd <bastien.dussard@gmail.com>
2023-12-07 18:51:06 +0100

    [IndividualBranch_t] changes into RelationsWithInductions for relations

--- include/ontologenius/core/ontoGraphs/Branchs/IndividualBranch.h
+++ include/ontologenius/core/ontoGraphs/Branchs/IndividualBranch.h
@@ -16,27 +16,16 @@ namespace ontologenius {
 // Classes predefinition
 class IndividualBranch_t;
 
-typedef Triplet_t<IndividualBranch_t, ObjectPropertyBranch_t, IndividualBranch_t> ObjectRelationTriplet_t;
-typedef Triplets<IndividualBranch_t, ObjectPropertyBranch_t, IndividualBranch_t> ObjectRelationTriplets;
-
 typedef Single_t<IndividualBranch_t*> IndividualElement_t;
 typedef Pair_t<ObjectPropertyBranch_t*, IndividualBranch_t*> IndivObjectRelationElement_t;
 typedef Pair_t<DataPropertyBranch_t*, LiteralNode*> IndivDataRelationElement_t;
 
 class IndividualBranch_t : public ValuedNode
 {
-  // as classes member, object property members, data property members can allow to induce class equiv, higher level class to pack everything
-  // instead of having std::vector<Triplets> object_properties_has_induced_; std::vector<Triplets> data_properties_has_induced_; std::vector<Triplets> is_a_has_induced;
-  // so we design a template class which can take all of these members to induce relations like std::vector<Induced_relations>
-
-  // reasons of equivalence inducing : 
-  // - obj prop card indiv
-  // - data prop card indiv
 public:
-  std::vector<ClassElement_t> is_a_;
-  std::vector<IndivObjectRelationElement_t> object_relations_;
-  std::vector<ObjectRelationTriplets> object_properties_has_induced_;
-  std::vector<IndivDataRelationElement_t> data_relations_;
+  RelationsWithInductions<ClassElement_t> is_a_;
+  RelationsWithInductions<IndivObjectRelationElement_t> object_relations_;
+  RelationsWithInductions<IndivDataRelationElement_t> data_relations_;
   std::vector<IndividualElement_t> same_as_;
   std::vector<IndividualElement_t> distinct_;
 
