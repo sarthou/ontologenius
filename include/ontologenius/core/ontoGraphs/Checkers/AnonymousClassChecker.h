9f568cd8a90b9506c19fac54f87e97f81caf339e
BastienDssrd <bastien.dussard@gmail.com>
2023-12-07 19:02:59 +0100

    [AnonymousClassChecker] adding the .h

--- include/ontologenius/core/ontoGraphs/Checkers/AnonymousClassChecker.h
+++ include/ontologenius/core/ontoGraphs/Checkers/AnonymousClassChecker.h
@@ -1,34 +1,52 @@
-#ifndef ONTOLOGENIUS_ANOCLASSCHECKER_H
-#define ONTOLOGENIUS_ANOCLASSCHECKER_H
+#ifndef ONTOLOGENIUS_ANONYMOUSCLASSCHECKER_H
+#define ONTOLOGENIUS_ANONYMOUSCLASSCHECKER_H
 
 #include "ontologenius/core/ontoGraphs/Checkers/ValidityChecker.h"
 #include "ontologenius/core/ontoGraphs/Graphs/AnonymousClassGraph.h"
 
-// check if the range of each property is disjoint with the domain of the children nodes
-// if the domains of elements at the same level are disjoint (i.e no intersection possible), do we reject it ?
 
 namespace ontologenius {
 
 class AnonymousClassChecker : public ValidityChecker<AnonymousClassBranch_t>
 {
 public:
-    explicit AnonymousClassChecker(AnonymousClassGraph* graph) : ValidityChecker(graph) {ano_class_graph_ = graph;}
-    ~AnonymousClassChecker() {}
+  explicit AnonymousClassChecker(AnonymousClassGraph* graph) : ValidityChecker(graph) {ano_class_graph_ = graph;}
+  ~AnonymousClassChecker() {}
 
-    size_t check() override;
-    void checkDisjoint();
-    void checkClassesDisjointness(ClassBranch_t* class_left, ClassBranch_t* class_right);
-    void compareDomainRange(AnonymousClassElement_t* node, std::vector<ClassElement_t> mother_ranges);
+  size_t check() override;
+  void checkDisjoint();
 
-    void printStatus()
-    {
-        ValidityChecker<AnonymousClassBranch_t>::printStatus("anonymous_class", "anonymous_classes", graph_vect_.size());
-    };
+  void printStatus()
+  {
+      ValidityChecker<AnonymousClassBranch_t>::printStatus("anonymous_class", "anonymous_classes", graph_vect_.size());
+  };
 
 private:
-    AnonymousClassGraph* ano_class_graph_;
+  AnonymousClassGraph* ano_class_graph_;
+  std::string current_ano_;
+
+  std::vector<std::string> resolveTree(AnonymousClassElement_t* ano_elem, const std::vector<ClassElement_t>& ranges);
+  std::vector<std::string> resolveTreeDataTypes(AnonymousClassElement_t* ano_elem); // Err
+
+  std::vector<std::string> checkPropertyDisjointness(AnonymousClassElement_t* ano_elem, const std::vector<ClassElement_t>& ranges);
+  void checkIntersectionDomainsDisjointess(AnonymousClassElement_t* ano_elem);
+  std::vector<std::string> checkRangeDomainDisjointness(AnonymousClassElement_t* ano_elem, const std::vector<ClassElement_t>& ranges);
+
+  std::vector<std::string> checkExpressionDisjointess(AnonymousClassElement_t* ano_elem, const std::vector<ClassElement_t>& ranges);
+  void checkObjectPropertyRangeDisjointness(AnonymousClassElement_t* ano_elem);
+  void checkDataPropertyRangeDisjointness(AnonymousClassElement_t* ano_elem);
+  
+  std::string checkClassesDisjointness(ClassBranch_t* class_left, ClassBranch_t* class_right); // Err
+  std::vector<std::string> checkClassesVectorDisjointness(const std::vector<ClassElement_t>& classes_left, const std::vector<ClassElement_t>& class_right);
+
+  template<typename T>
+  std::vector<std::string> checkPropertyDomainDisjointness(const T& property, const std::vector<ClassElement_t>& ranges)
+  {
+    return checkClassesVectorDisjointness(property->domains_, ranges);
+  }
+
 };
 
 } // namespace ontologenius
 
-#endif // ONTOLOGENIUS_ANOCLASSCHECKER_H
+#endif // ONTOLOGENIUS_ANONYMOUSCLASSCHECKER_H
