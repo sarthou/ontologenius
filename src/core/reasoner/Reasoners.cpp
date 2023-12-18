69dbe37d00ca3d9f372c9dd550b05b81024a6d24
Guillaume Sarthou <gsarthou@laas.fr>
2023-12-18 11:18:54 +0100

    [Reasoners] remove useless inclusion

--- src/core/reasoner/Reasoners.cpp
+++ src/core/reasoner/Reasoners.cpp
@@ -8,7 +8,7 @@
 namespace ontologenius {
 
 Reasoners::Reasoners(const std::string& agent_name, Ontology* onto) : agent_name_(agent_name),
-                                                                      loader_("ontologenius", "ReasonerInterface")
+                                                                      loader_("ontologenius", "ontologenius::ReasonerInterface")
 {
   ontology_ = onto;
 }
