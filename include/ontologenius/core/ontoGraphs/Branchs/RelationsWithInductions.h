5f6710353a6dec1047604aa6d9e3d971f2f03983
BastienDssrd <bastien.dussard@gmail.com>
2023-12-21 15:00:51 +0100

    [RelationsWithInductions] change from reference to pointers on Triplets

--- include/ontologenius/core/ontoGraphs/Branchs/RelationsWithInductions.h
+++ include/ontologenius/core/ontoGraphs/Branchs/RelationsWithInductions.h
@@ -22,8 +22,8 @@ class RelationsWithInductions
 {
 public:
 	std::vector<T> relations;
-	std::vector<ObjectRelationTriplets> has_induced_object_relations;
-	std::vector<InheritedRelationTriplets> has_induced_inheritance_relations;
+	std::vector<ObjectRelationTriplets*> has_induced_object_relations;
+	std::vector<InheritedRelationTriplets*> has_induced_inheritance_relations;
 
 	size_t size() { return relations.size(); }
 	T& operator[](size_t index) { return relations[index]; }
@@ -31,23 +31,25 @@ public:
 	size_t push_back(T& relation)
 	{
 		relations.emplace_back(relation);
-		has_induced_object_relations.emplace_back();
-		has_induced_inheritance_relations.emplace_back();
+		has_induced_object_relations.emplace_back(new ObjectRelationTriplets);
+		has_induced_inheritance_relations.emplace_back(new InheritedRelationTriplets);
 		return relations.size() -1;
 	}
 
 	template< class... Args >
 	T& emplace_back(Args&&... args)
 	{
-		has_induced_object_relations.emplace_back();
-		has_induced_inheritance_relations.emplace_back();
+		has_induced_object_relations.emplace_back(new ObjectRelationTriplets);
+		has_induced_inheritance_relations.emplace_back(new InheritedRelationTriplets);
 		return relations.emplace_back(std::forward<Args>(args)...);
 	}
 
 	void erase(size_t index)
 	{
 		relations.erase(relations.begin() + index);
+		delete has_induced_object_relations.at(index);
 		has_induced_object_relations.erase(has_induced_object_relations.begin() + index);
+		delete has_induced_inheritance_relations.at(index);
 		has_induced_inheritance_relations.erase(has_induced_inheritance_relations.begin() + index);
 	}
 
