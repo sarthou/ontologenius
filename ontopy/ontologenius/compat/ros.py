42096e5428ffafcd5687759bd465ae176b63e976
Adrien Vigne <adrien.vigne@laas.fr>
2024-01-12 17:24:22 +0100

    [Compat] add ros2 part

--- ontopy/ontologenius/compat/ros.py
+++ ontopy/ontologenius/compat/ros.py
@@ -56,6 +56,100 @@ if os.environ["ROS_VERSION"] == "1":
 
     print("v1")
 elif os.environ["ROS_VERSION"] == "2":
-    import rclpy 
+
+    import rclpy
+    from rclpy.node import Node
+    from threading import Lock
+    from rclpy.service import SrvTypeRequest, SrvTypeResponse
+    from rclpy.client import Client
+    from rclpy.publisher import Publisher
+    from rclpy.subscription import Subscription
+    from rclpy.time import Time
+
+
+    class SingletonMeta(type):
+        """
+        This is a thread-safe implementation of Singleton.
+        """
+
+        _instances = {}
+
+        _lock: Lock = Lock()
+        """
+        We now have a lock object that will be used to synchronize threads during
+        first access to the Singleton.
+        """
+
+        def __call__(cls, *args, **kwargs):
+            """
+            Possible changes to the value of the `__init__` argument do not affect
+            the returned instance.
+            """
+            # Now, imagine that the program has just been launched. Since there's no
+            # Singleton instance yet, multiple threads can simultaneously pass the
+            # previous conditional and reach this point almost at the same time. The
+            # first of them will acquire lock and will proceed further, while the
+            # rest will wait here.
+            with cls._lock:
+                # The first thread to acquire the lock, reaches this conditional,
+                # goes inside and creates the Singleton instance. Once it leaves the
+                # lock block, a thread that might have been waiting for the lock
+                # release may then enter this section. But since the Singleton field
+                # is already initialized, the thread won't create a new object.
+                if cls not in cls._instances:
+                    instance = super().__call__(*args, **kwargs)
+                    cls._instances[cls] = instance
+            return cls._instances[cls]
+
 
     print("v2")
+
+
+    class OntoService:
+        def __init__(self, client: Client, node_: Node):
+            self.client: Client = client
+            self.node_: Node = node_
+
+        def call(self, params: SrvTypeRequest) -> SrvTypeResponse:
+            future = self.client.call_async(request=params)
+            rclpy.spin_until_future_complete(self.node_, future)
+            return future.result()
+
+
+    class OntoPublisher:
+        def __init__(self, pub):
+            self.pub: Publisher = pub
+
+        def publish(self, msg):
+            self.pub.publish(msg)
+
+
+    class OntoSubscriber:
+        def __init__(self, sub):
+            self.sub: Subscription = sub
+
+
+    class OntoROS(Node, metaclass=SingletonMeta):
+
+        def __init__(self):
+            super().__init__("OntoRos")
+
+        @staticmethod
+        def createService(srv_name, srv_type, connected) -> OntoService:
+            return OntoService(OntoROS().create_client(srv_name, srv_type), OntoROS())
+
+        @staticmethod
+        def createPublisher(pub_name, pub_type, queue_size: int) -> OntoPublisher:
+            return OntoPublisher(OntoROS().create_publisher(pub_type, pub_name, qos_profile=queue_size))
+
+        @staticmethod
+        def createSubscriber(sub_name, sub_type, callback, queue_size: int = 10) -> OntoSubscriber:
+            return OntoSubscriber(OntoROS().create_subscription(sub_type, sub_name, callback, qos_profile=queue_size))
+
+        @staticmethod
+        def getRosTime() -> Time:
+            return OntoROS().get_clock().now()
+
+        @staticmethod
+        def isShutdown() -> bool:
+            return not rclpy.ok()
