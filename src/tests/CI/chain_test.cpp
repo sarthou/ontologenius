bccf84e4f0dd90ba82c97ba55a00eb2d2642ede0
Guillaume Sarthou <gsarthou@laas.fr>
2024-02-07 11:12:06 +0100

    [CI] chain test chain break due to inheritance removal

--- src/tests/CI/chain_test.cpp
+++ src/tests/CI/chain_test.cpp
@@ -185,6 +185,42 @@ TEST(chain_tests, chain_deletion_same_as)
   onto_ptr->feeder.waitUpdate(1000);
 }
 
+TEST(chain_tests, chain_deletion_inheritage)
+{
+  std::vector<std::string> res;
+  onto_ptr->actions.reset();
+  onto_ptr->actions.close();
+  onto_ptr->reasoners.activate("ontologenius::ReasonerChain");
+
+  res = onto_ptr->individuals.getOn("ball", "isIn");
+  EXPECT_TRUE(res.empty());
+
+  res = onto_ptr->individuals.getOn("ball", "thirdChain");
+  EXPECT_TRUE(res.empty());
+
+  onto_ptr->feeder.addProperty("ball", "isOnTop", "cube_base_bis");
+  onto_ptr->feeder.waitUpdate(1000);
+
+  res = onto_ptr->individuals.getOn("ball", "isIn");
+  EXPECT_TRUE(find(res.begin(), res.end(), "box") != res.end());
+
+  res = onto_ptr->individuals.getOn("ball", "thirdChain");
+  EXPECT_TRUE(find(res.begin(), res.end(), "table") != res.end());
+
+  onto_ptr->feeder.removeInheritage("isOnTop", "isOn");
+  onto_ptr->feeder.waitUpdate(1000);
+
+  res = onto_ptr->individuals.getOn("ball", "isIn");
+  EXPECT_TRUE(res.empty());
+
+  res = onto_ptr->individuals.getOn("ball", "thirdChain");
+  EXPECT_TRUE(res.empty());
+
+  onto_ptr->feeder.addInheritage("isOnTop", "isOn");
+  onto_ptr->feeder.removeProperty("ball", "isOnTop", "cube_base_bis");
+  onto_ptr->feeder.waitUpdate(1000);
+}
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "ontologenius_chain_inference");
