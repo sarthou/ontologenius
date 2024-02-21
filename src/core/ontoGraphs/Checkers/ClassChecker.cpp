887599744daa340b3737ad31cec78473dc7a764b
Guillaume Sarthou <gsarthou@laas.fr>
2024-02-21 16:21:30 +0100

    [ClassChecker] refact

--- src/core/ontoGraphs/Checkers/ClassChecker.cpp
+++ src/core/ontoGraphs/Checkers/ClassChecker.cpp
@@ -10,15 +10,21 @@ namespace ontologenius {
 size_t ClassChecker::check()
 {
   std::shared_lock<std::shared_timed_mutex> lock(class_graph_->mutex_);
-  graph_size = graph_vect_.size();
+  std::unordered_set<ClassBranch_t*> up;
 
-  checkDisjoint();
+  for(auto& _class : graph_vect_)
+  {
+    class_graph_->getUpPtr(_class, up);
 
-  checkObjectPropertyDomain();
-  checkObjectPropertyRange();
+    checkDisjoint(_class, up);
+    checkObjectPropertyDomain(_class, up);
+    checkObjectPropertyRange(_class);
 
-  checkDataPropertyDomain();
-  checkDataPropertyRange();
+    checkDataPropertyDomain(_class, up);
+    checkDataPropertyRange(_class);
+
+    up.clear();
+  }
 
   is_analysed = true;
   printStatus();
@@ -26,137 +32,107 @@ size_t ClassChecker::check()
   return getErrors();
 }
 
-void ClassChecker::checkDisjoint()
+void ClassChecker::checkDisjoint(ClassBranch_t* branch, std::unordered_set<ClassBranch_t*> up)
 {
-  for(ClassBranch_t* branch : graph_vect_)
+  auto intersection = class_graph_->isDisjoint(up, up);
+  if(intersection != nullptr)
   {
-    std::unordered_set<ClassBranch_t*> up;
-    class_graph_->getUpPtr(branch, up);
+    ClassBranch_t* disjoint_with = class_graph_->firstIntersection(up, intersection->disjoints_);
 
-    auto intersection = class_graph_->isDisjoint(up, up);
-    if(intersection != nullptr)
-    {
-      ClassBranch_t* disjoint_with = class_graph_->firstIntersection(up, intersection->disjoints_);
-
-      if(disjoint_with != nullptr)
-        print_error("'" + branch->value() + "' can't be a '" + intersection->value() + "' and a '"
-        + disjoint_with->value() + "' because of disjonction between classes '"
-        + intersection->value() + "' and '" + disjoint_with->value() + "'");
-      else
-        print_error("'" + branch->value() + "' can't be a '" + intersection->value() + "' because of disjonction");
-    }
+    if(disjoint_with != nullptr)
+      print_error("'" + branch->value() + "' can't be a '" + intersection->value() + "' and a '"
+      + disjoint_with->value() + "' because of disjonction between classes '"
+      + intersection->value() + "' and '" + disjoint_with->value() + "'");
+    else
+      print_error("'" + branch->value() + "' can't be a '" + intersection->value() + "' because of disjonction");
   }
 }
 
-void ClassChecker::checkObjectPropertyDomain()
+void ClassChecker::checkObjectPropertyDomain(ClassBranch_t* branch, std::unordered_set<ClassBranch_t*> up)
 {
-  std::shared_lock<std::shared_timed_mutex> lock(class_graph_->mutex_);
-  for(auto& _class : graph_vect_)
+  for(ClassObjectRelationElement_t& object_relation : branch->object_relations_)
   {
-    std::unordered_set<ClassBranch_t*> up;
-    class_graph_->getUpPtr(_class, up);
+    std::unordered_set<ClassBranch_t*> domain;
+    class_graph_->object_property_graph_->getDomainPtr(object_relation.first, domain, 0);
 
-    for(ClassObjectRelationElement_t& object_relation : _class->object_relations_)
+    if(domain.size() != 0)
     {
-      std::unordered_set<ClassBranch_t*> domain;
-      class_graph_->object_property_graph_->getDomainPtr(object_relation.first, domain, 0);
-
-      if(domain.size() != 0)
+      auto intersection = class_graph_->checkDomainOrRange(domain, up);
+      if(intersection.first == false)
       {
-        auto intersection = class_graph_->checkDomainOrRange(domain, up);
-        if(intersection.first == false)
+        if(intersection.second == nullptr)
         {
-          if(intersection.second == nullptr)
-          {
-            _class->flags_["domain"].push_back(object_relation.first->value());
-            print_warning("'" + _class->value() + "' is not in domain of '" + object_relation.first->value() + "'");
-          }
-          else
-            print_error("'" + _class->value() + "' can not be in domain of '" + object_relation.first->value() + "'");
+          branch->flags_["domain"].push_back(object_relation.first->value());
+          print_warning("'" + branch->value() + "' is not in domain of '" + object_relation.first->value() + "'");
         }
+        else
+          print_error("'" + branch->value() + "' can not be in domain of '" + object_relation.first->value() + "'");
       }
     }
   }
 }
 
-void ClassChecker::checkObjectPropertyRange()
+void ClassChecker::checkObjectPropertyRange(ClassBranch_t* branch)
 {
-  std::shared_lock<std::shared_timed_mutex> lock(class_graph_->mutex_);
-
-  for(auto& _class : graph_vect_)
+  for(ClassObjectRelationElement_t& object_relation : branch->object_relations_)
   {
-    for(ClassObjectRelationElement_t& object_relation : _class->object_relations_)
+    std::unordered_set<ClassBranch_t*> range;
+    class_graph_->object_property_graph_->getRangePtr(object_relation.first, range, 0);
+    
+    if(range.size() != 0)
     {
-      std::unordered_set<ClassBranch_t*> range;
-      class_graph_->object_property_graph_->getRangePtr(object_relation.first, range, 0);
-     
-      if(range.size() != 0)
-      {
-        std::unordered_set<ClassBranch_t*> up;
-        class_graph_->getUpPtr(object_relation.second, up);
+      std::unordered_set<ClassBranch_t*> up;
+      class_graph_->getUpPtr(object_relation.second, up);
 
-        auto intersection = class_graph_->checkDomainOrRange(range, up);
-        if(intersection.first == false)
+      auto intersection = class_graph_->checkDomainOrRange(range, up);
+      if(intersection.first == false)
+      {
+        if(intersection.second == nullptr)
         {
-          if(intersection.second == nullptr)
-          {
-            _class->flags_["range"].push_back(object_relation.first->value());
-            print_warning("'" + object_relation.second->value() + "' is not in range of '" + object_relation.first->value() + "'");
-          }
-          else
-            print_error("'" + object_relation.second->value() + "' can not be in range of '" + object_relation.first->value() + "'");
+          branch->flags_["range"].push_back(object_relation.first->value());
+          print_warning("'" + object_relation.second->value() + "' is not in range of '" + object_relation.first->value() + "'");
         }
+        else
+          print_error("'" + object_relation.second->value() + "' can not be in range of '" + object_relation.first->value() + "'");
       }
     }
   }
 }
 
-void ClassChecker::checkDataPropertyDomain()
+void ClassChecker::checkDataPropertyDomain(ClassBranch_t* branch, std::unordered_set<ClassBranch_t*> up)
 {
-  std::shared_lock<std::shared_timed_mutex> lock(class_graph_->mutex_);
-
-  for(auto& _class : graph_vect_)
+  for(ClassDataRelationElement_t& relation : branch->data_relations_)
   {
-    std::unordered_set<ClassBranch_t*> up;
-    class_graph_->getUpPtr(_class, up);
+    std::unordered_set<ClassBranch_t*> domain;
+    class_graph_->data_property_graph_->getDomainPtr(relation.first, domain, 0);
 
-    for(ClassDataRelationElement_t& relation : _class->data_relations_)
+    if(domain.size() != 0)
     {
-      std::unordered_set<ClassBranch_t*> domain;
-      class_graph_->data_property_graph_->getDomainPtr(relation.first, domain, 0);
-
-      if(domain.size() != 0)
+      auto intersection = class_graph_->checkDomainOrRange(domain, up);
+      if(intersection.first == false)
       {
-        auto intersection = class_graph_->checkDomainOrRange(domain, up);
-        if(intersection.first == false)
+        if(intersection.second == nullptr)
         {
-          if(intersection.second == nullptr)
-          {
-            _class->flags_["domain"].push_back(relation.first->value());
-            print_warning("'" + _class->value() + "' is not in domain of '" + relation.first->value() + "'");
-          }
-          else
-            print_error("'" + _class->value() + "' can not be in domain of '" + relation.first->value() + "'");
+          branch->flags_["domain"].push_back(relation.first->value());
+          print_warning("'" + branch->value() + "' is not in domain of '" + relation.first->value() + "'");
         }
+        else
+          print_error("'" + branch->value() + "' can not be in domain of '" + relation.first->value() + "'");
       }
     }
   }
 }
 
-void ClassChecker::checkDataPropertyRange()
+void ClassChecker::checkDataPropertyRange(ClassBranch_t* branch)
 {
-  std::shared_lock<std::shared_timed_mutex> lock(class_graph_->mutex_);
-  for(auto& _class : graph_vect_)
+  for(ClassDataRelationElement_t& relation : branch->data_relations_)
   {
-    for(ClassDataRelationElement_t& relation : _class->data_relations_)
+    std::unordered_set<std::string> range = class_graph_->data_property_graph_->getRange(relation.first->value());
+    if(range.size() != 0)
     {
-      std::unordered_set<std::string> range = class_graph_->data_property_graph_->getRange(relation.first->value());
-      if(range.size() != 0)
-      {
-        auto intersection = range.find(relation.second->type_);
-        if(intersection == range.end())
-          print_error("'" + relation.second->type_ + "' is not in range of '" + relation.first->value() + "'");
-      }
+      auto intersection = range.find(relation.second->type_);
+      if(intersection == range.end())
+        print_error("'" + relation.second->type_ + "' is not in range of '" + relation.first->value() + "'");
     }
   }
 }
