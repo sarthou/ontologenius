2b29f35c03088fefb8a0d116c73a892d2e4bebbd
Guillaume Sarthou <gsarthou@laas.fr>
2024-05-29 18:10:21 +0200

    [clang-format] refact

--- include/ontologenius/API/ontologenius/clients/ClientBase.h
+++ include/ontologenius/API/ontologenius/clients/ClientBase.h
@@ -1,71 +1,71 @@
 #ifndef ONTOLOGENIUS_CLIENTBASE_H
 #define ONTOLOGENIUS_CLIENTBASE_H
 
-#include <vector>
 #include <string>
+#include <vector>
 
 #include "ontologenius/compat/ros.h"
 
 #ifndef COLOR_OFF
-#define COLOR_OFF     "\x1B[0m"
+#define COLOR_OFF "\x1B[0m"
 #endif
 #ifndef COLOR_RED
-#define COLOR_RED     "\x1B[0;91m"
+#define COLOR_RED "\x1B[0;91m"
 #endif
 #ifndef COLOR_ORANGE
-#define COLOR_ORANGE  "\x1B[1;33m"
+#define COLOR_ORANGE "\x1B[1;33m"
 #endif
 #ifndef COLOR_GREEN
-#define COLOR_GREEN   "\x1B[1;92m"
+#define COLOR_GREEN "\x1B[1;92m"
 #endif
 
 namespace onto {
 
-/// @brief The ClientBase class provides an abstraction for any ROS services.
-/// This class ensure a persistent connection with the service based on.
-/// The persistent connection ensures a minimal response time.
-/// A reconnection logic is implemented in the event that the persistent connection fails.
-class ClientBase
-{
-  static int16_t ignore_;
-public:
-  /// @brief Constructs a ROS client.
-  /// @param name is the name of the ontologenius service
-  explicit ClientBase(const std::string& name) : name_(name),
-                                                 error_code_(0),
-                                                 client_("/ontologenius/" + name)
-
-  {}
-
-  /// @brief Gives the total number of service calls from all ClientBase instances since the last reset.
-  size_t nb() {return cpt;}
-  /// @brief Reset Call Counter for all instances of ClientBase.
-  void resetNb() {cpt = 0;}
-  static void verbose(bool verbose) { verbose_ = verbose; }
-
-  int getErrorCode() { return error_code_; }
-
-  /// @brief Calls the service set up in the constructor of ClientBase.
-  /// @param action the query action.
-  /// @param param the query parameters.
-  /// @return Returns a list of string. If the service call fails, the first element of the returned vector is "ERR:SERVICE_FAIL".
-  inline std::vector<std::string> call(const std::string& action, const std::string& param, int16_t& code = ignore_)
+  /// @brief The ClientBase class provides an abstraction for any ROS services.
+  /// This class ensure a persistent connection with the service based on.
+  /// The persistent connection ensures a minimal response time.
+  /// A reconnection logic is implemented in the event that the persistent connection fails.
+  class ClientBase
   {
-    cpt++;
+    static int16_t ignore_;
 
-    auto req = ontologenius::compat::make_request<ontologenius::compat::OntologeniusService>();
-    auto res = ontologenius::compat::make_response<ontologenius::compat::OntologeniusService>();
+  public:
+    /// @brief Constructs a ROS client.
+    /// @param name is the name of the ontologenius service
+    explicit ClientBase(const std::string& name) : name_(name),
+                                                   error_code_(0),
+                                                   client_("/ontologenius/" + name)
 
-    [action, param](auto&& req)
+    {}
+
+    /// @brief Gives the total number of service calls from all ClientBase instances since the last reset.
+    size_t nb() { return cpt; }
+    /// @brief Reset Call Counter for all instances of ClientBase.
+    void resetNb() { cpt = 0; }
+    static void verbose(bool verbose) { verbose_ = verbose; }
+
+    int getErrorCode() const { return error_code_; }
+
+    /// @brief Calls the service set up in the constructor of ClientBase.
+    /// @param action the query action.
+    /// @param param the query parameters.
+    /// @return Returns a list of string. If the service call fails, the first element of the returned vector is "ERR:SERVICE_FAIL".
+    inline std::vector<std::string> call(const std::string& action, const std::string& param, int16_t& code = ignore_)
     {
-      req->action = action;
-      req->param = param;
-    }(ontologenius::compat::onto_ros::getServicePointer(req));
+      cpt++;
 
-    using ResultTy = typename decltype(client_)::Status;
+      auto req = ontologenius::compat::make_request<ontologenius::compat::OntologeniusService>();
+      auto res = ontologenius::compat::make_response<ontologenius::compat::OntologeniusService>();
 
-    switch (client_.call(req, res))
-    {
+      [action, param](auto&& req) {
+        req->action = action;
+        req->param = param;
+      }(ontologenius::compat::onto_ros::getServicePointer(req));
+
+      using ResultTy = typename decltype(client_)::Status;
+
+      switch(client_.call(req, res))
+      {
       case ResultTy::SUCCESSFUL_WITH_RETRIES:
       {
         if(verbose_)
@@ -74,8 +74,7 @@ public:
       }
       case ResultTy::SUCCESSFUL:
       {
-        return [&](auto&& res)
-        {
+        return [&](auto&& res) {
           code = res->code;
           error_code_ = res->code;
           return res->values;
@@ -88,51 +87,51 @@ public:
         error_code_ = -1;
         if(verbose_)
           std::cout << COLOR_RED << "Failure to call ontologenius/" << name_ << COLOR_OFF << std::endl;
-        return { "ERR:SERVICE_FAIL" };
+        return {"ERR:SERVICE_FAIL"};
+      }
       }
     }
-  }
 
-  /// @brief Calls the service set up in the constructor of ClientBase.
-  /// @param action the query action.
-  /// @param param the query parameters.
-  /// @return Returns a single string. If the service call fails, the returned value is "ERR:SERVICE_FAIL".
-  inline std::string callStr(const std::string& action, const std::string& param, int16_t& code = ignore_)
-  {
-    auto res = this->call(action, param, code);
-    return res.empty() ? "" : res[0];
-  }
+    /// @brief Calls the service set up in the constructor of ClientBase.
+    /// @param action the query action.
+    /// @param param the query parameters.
+    /// @return Returns a single string. If the service call fails, the returned value is "ERR:SERVICE_FAIL".
+    inline std::string callStr(const std::string& action, const std::string& param, int16_t& code = ignore_)
+    {
+      auto res = this->call(action, param, code);
+      return res.empty() ? "" : res[0];
+    }
 
-  /// @brief Calls the service set up in the constructor of ClientBase.
-  /// @param action the query action.
-  /// @param param the query parameters.
-  /// @return Returns false if the service call fails.
-  inline bool callNR(const std::string& action, const std::string& param)
-  {
-    return this->callStr(action, param) != "ERR:SERVICE_FAIL";
-  }
+    /// @brief Calls the service set up in the constructor of ClientBase.
+    /// @param action the query action.
+    /// @param param the query parameters.
+    /// @return Returns false if the service call fails.
+    inline bool callNR(const std::string& action, const std::string& param)
+    {
+      return this->callStr(action, param) != "ERR:SERVICE_FAIL";
+    }
 
-  /// @brief Calls the service set up in the constructor of ClientBase.
-  /// @param action the query action.
-  /// @param param the query parameters.
-  /// @return Returns false if the service call fails or the result code of the service is different from SUCCESS.
-  inline bool callBool(const std::string& action, const std::string& param)
-  {
-    int16_t code;
-    auto res = this->callStr(action, param, code);
+    /// @brief Calls the service set up in the constructor of ClientBase.
+    /// @param action the query action.
+    /// @param param the query parameters.
+    /// @return Returns false if the service call fails or the result code of the service is different from SUCCESS.
+    inline bool callBool(const std::string& action, const std::string& param)
+    {
+      int16_t code = 0;
+      auto res = this->callStr(action, param, code);
 
-    return (res != "ERR:SERVICE_FAIL") && (code == 0);
-  }
+      return (res != "ERR:SERVICE_FAIL") && (code == 0);
+    }
 
-private:
-  std::string name_;
-  static size_t cpt;
-  static bool verbose_;
-  int error_code_;
+  private:
+    std::string name_;
+    static size_t cpt;
+    static bool verbose_;
+    int error_code_;
 
-protected:
-  ontologenius::compat::onto_ros::Client<ontologenius::compat::OntologeniusService> client_;
-};
+  protected:
+    ontologenius::compat::onto_ros::Client<ontologenius::compat::OntologeniusService> client_;
+  };
 
 } // namespace onto
 
--- include/ontologenius/API/ontologenius/clientsIndex/ClientBaseIndex.h
+++ include/ontologenius/API/ontologenius/clientsIndex/ClientBaseIndex.h
@@ -1,70 +1,71 @@
 #ifndef ONTOLOGENIUS_CLIENTBASEINDEX_H
 #define ONTOLOGENIUS_CLIENTBASEINDEX_H
 
-#include <vector>
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "ontologenius/compat/ros.h"
 
 #ifndef COLOR_OFF
-#define COLOR_OFF     "\x1B[0m"
+#define COLOR_OFF "\x1B[0m"
 #endif
 #ifndef COLOR_RED
-#define COLOR_RED     "\x1B[0;91m"
+#define COLOR_RED "\x1B[0;91m"
 #endif
 #ifndef COLOR_ORANGE
-#define COLOR_ORANGE  "\x1B[1;33m"
+#define COLOR_ORANGE "\x1B[1;33m"
 #endif
 #ifndef COLOR_GREEN
-#define COLOR_GREEN   "\x1B[1;92m"
+#define COLOR_GREEN "\x1B[1;92m"
 #endif
 
 namespace onto {
 
-/// @brief The ClientBaseIndex class provides an abstraction for any ROS services based on indexes.
-/// This class ensure a persistent connection with the service based on.
-/// The persistent connection ensures a minimal response time.
-/// A reconnection logic is implemented in the event that the persistent connection fails. 
-class ClientBaseIndex
-{
-  static int16_t ignore_;
-public:
-  /// @brief Constructs a ROS client.
-  /// @param name is the name of the ontologenius service
-  explicit ClientBaseIndex(const std::string& name) : name_(name),
-                                                      error_code_(0),
-                                                      client_("/ontologenius/" + name)
-  {}
-
-  /// @brief Gives the total number of service calls from all ClientBaseIndex instances since the last reset.
-  size_t nb() {return cpt;}
-  /// @brief Reset Call Counter for all instances of ClientBaseIndex.
-  void resetNb() {cpt = 0;}
-  static void verbose(bool verbose) { verbose_ = verbose; }
-
-  int getErrorCode() { return error_code_; }
-
-  /// @brief Calls the service set up in the constructor of ClientBaseIndex.
-  /// @param action the query action.
-  /// @param param the query parameters.
-  /// @return Returns a list of string. If the service call fails, the first element of the returned vector is "ERR:SERVICE_FAIL".
-  inline std::vector<std::string> call(const std::string& action, const std::string& param, int16_t &code = ignore_)
+  /// @brief The ClientBaseIndex class provides an abstraction for any ROS services based on indexes.
+  /// This class ensure a persistent connection with the service based on.
+  /// The persistent connection ensures a minimal response time.
+  /// A reconnection logic is implemented in the event that the persistent connection fails.
+  class ClientBaseIndex
   {
-    cpt++;
+    static int16_t ignore_;
 
-    auto req = ontologenius::compat::make_request<ontologenius::compat::OntologeniusIndexService>();
-    auto res = ontologenius::compat::make_response<ontologenius::compat::OntologeniusIndexService>();
+  public:
+    /// @brief Constructs a ROS client.
+    /// @param name is the name of the ontologenius service
+    explicit ClientBaseIndex(const std::string& name) : name_(name),
+                                                        error_code_(0),
+                                                        client_("/ontologenius/" + name)
+    {}
 
-    [action, param](auto&& req)
+    /// @brief Gives the total number of service calls from all ClientBaseIndex instances since the last reset.
+    size_t nb() { return cpt; }
+    /// @brief Reset Call Counter for all instances of ClientBaseIndex.
+    void resetNb() { cpt = 0; }
+    static void verbose(bool verbose) { verbose_ = verbose; }
+
+    int getErrorCode() const { return error_code_; }
+
+    /// @brief Calls the service set up in the constructor of ClientBaseIndex.
+    /// @param action the query action.
+    /// @param param the query parameters.
+    /// @return Returns a list of string. If the service call fails, the first element of the returned vector is "ERR:SERVICE_FAIL".
+    inline std::vector<std::string> call(const std::string& action, const std::string& param, int16_t& code = ignore_)
     {
-      req->action = action;
-      req->param = param;
-    }(ontologenius::compat::onto_ros::getServicePointer(req));
+      cpt++;
 
-    using ResultTy = typename decltype(client_)::Status;
+      auto req = ontologenius::compat::make_request<ontologenius::compat::OntologeniusIndexService>();
+      auto res = ontologenius::compat::make_response<ontologenius::compat::OntologeniusIndexService>();
 
-    switch (client_.call(req, res)) {
+      [action, param](auto&& req) {
+        req->action = action;
+        req->param = param;
+      }(ontologenius::compat::onto_ros::getServicePointer(req));
+
+      using ResultTy = typename decltype(client_)::Status;
+
+      switch(client_.call(req, res))
+      {
       case ResultTy::SUCCESSFUL_WITH_RETRIES:
       {
         if(verbose_)
@@ -73,8 +74,7 @@ public:
       }
       case ResultTy::SUCCESSFUL:
       {
-        return [&](auto&& res)
-        {
+        return [&](auto&& res) {
           code = res->code;
           error_code_ = res->code;
           return res->string_values;
@@ -88,32 +88,31 @@ public:
       }
       default:
       {
-        return { "ERR:SERVICE_FAIL" };
+        return {"ERR:SERVICE_FAIL"};
+      }
       }
     }
-  }
 
-  /// @brief Calls the service set up in the constructor of ClientBaseIndex.
-  /// @param action the query action.
-  /// @param param the query parameters.
-  /// @return Returns a list of int64. If the service call fails, the first element of the returned vector is "0".
-  inline std::vector<int64_t> callIndexes(const std::string& action, const std::string& param, int16_t &code = ignore_)
-  {
-    cpt++;
-
-    auto req = ontologenius::compat::make_request<ontologenius::compat::OntologeniusIndexService>();
-    auto res = ontologenius::compat::make_response<ontologenius::compat::OntologeniusIndexService>();
-
-    [action, param](auto&& req)
+    /// @brief Calls the service set up in the constructor of ClientBaseIndex.
+    /// @param action the query action.
+    /// @param param the query parameters.
+    /// @return Returns a list of int64. If the service call fails, the first element of the returned vector is "0".
+    inline std::vector<int64_t> callIndexes(const std::string& action, const std::string& param, int16_t& code = ignore_)
     {
-      req->action = action;
-      req->param = param;
-    }(ontologenius::compat::onto_ros::getServicePointer(req));
+      cpt++;
 
-    using ResultTy = typename decltype(client_)::Status;
+      auto req = ontologenius::compat::make_request<ontologenius::compat::OntologeniusIndexService>();
+      auto res = ontologenius::compat::make_response<ontologenius::compat::OntologeniusIndexService>();
 
-    switch (client_.call(req, res))
-    {
+      [action, param](auto&& req) {
+        req->action = action;
+        req->param = param;
+      }(ontologenius::compat::onto_ros::getServicePointer(req));
+
+      using ResultTy = typename decltype(client_)::Status;
+
+      switch(client_.call(req, res))
+      {
       case ResultTy::SUCCESSFUL_WITH_RETRIES:
       {
         if(verbose_)
@@ -122,8 +121,7 @@ public:
       }
       case ResultTy::SUCCESSFUL:
       {
-        return [&](auto&& res)
-        {
+        return [&](auto&& res) {
           code = res->code;
           error_code_ = res->code;
           return res->index_values;
@@ -137,59 +135,60 @@ public:
       }
       default:
       {
-        return { 0 };
+        return {0};
+      }
       }
     }
-  }
 
-  /// @brief Calls the service set up in the constructor of ClientBaseIndex.
-  /// @param action the query action.
-  /// @param param the query parameters.
-  /// @return Returns a single string. If the service call fails, the returned value is "ERR:SERVICE_FAIL".
-  inline std::string callStr(const std::string& action, const std::string& param, int16_t& code = ignore_)
-  {
-    auto res = this->call(action, param, code);
-    return res.empty() ? "" : res[0];
-  }
+    /// @brief Calls the service set up in the constructor of ClientBaseIndex.
+    /// @param action the query action.
+    /// @param param the query parameters.
+    /// @return Returns a single string. If the service call fails, the returned value is "ERR:SERVICE_FAIL".
+    inline std::string callStr(const std::string& action, const std::string& param, int16_t& code = ignore_)
+    {
+      auto res = this->call(action, param, code);
+      return res.empty() ? "" : res[0];
+    }
 
-  /// @brief Calls the service set up in the constructor of ClientBaseIndex.
-  /// @param action the query action.
-  /// @param param the query parameters.
-  /// @return Returns a single int64. If the service call fails or has no response, the returned value is "0".
-  inline int64_t callIndex(const std::string& action, const std::string& param)
-  {
-    auto res = this->callIndexes(action, param);
-    return res.empty() ? 0 : res[0];
-  }
+    /// @brief Calls the service set up in the constructor of ClientBaseIndex.
+    /// @param action the query action.
+    /// @param param the query parameters.
+    /// @return Returns a single int64. If the service call fails or has no response, the returned value is "0".
+    inline int64_t callIndex(const std::string& action, const std::string& param)
+    {
+      auto res = this->callIndexes(action, param);
+      return res.empty() ? 0 : res[0];
+    }
 
-  /// @brief Calls the service set up in the constructor of ClientBaseIndex.
-  /// @param action the query action.
-  /// @param param the query parameters.
-  /// @return Returns false if the service call fails.
-  inline bool callNR(const std::string& action, const std::string& param)
-  {
-    return this->callStr(action, param) != "ERR:SERVICE_FAIL";
-  }
+    /// @brief Calls the service set up in the constructor of ClientBaseIndex.
+    /// @param action the query action.
+    /// @param param the query parameters.
+    /// @return Returns false if the service call fails.
+    inline bool callNR(const std::string& action, const std::string& param)
+    {
+      return this->callStr(action, param) != "ERR:SERVICE_FAIL";
+    }
 
-  /// @brief Calls the service set up in the constructor of ClientBaseIndex.
-  /// @param action the query action.
-  /// @param param the query parameters.
-  /// @return Returns false if the service call fails or the result code of the service is different from SUCCESS.
-  inline bool callBool(const std::string& action, const std::string& param)
-  {
-    int16_t code;
-    auto res = this->callStr(action, param, code);
-    return (res != "ERR:SERVICE_FAIL") && (code == 0);
-  }
+    /// @brief Calls the service set up in the constructor of ClientBaseIndex.
+    /// @param action the query action.
+    /// @param param the query parameters.
+    /// @return Returns false if the service call fails or the result code of the service is different from SUCCESS.
+    inline bool callBool(const std::string& action, const std::string& param)
+    {
+      int16_t code = 0;
+      auto res = this->callStr(action, param, code);
+      return (res != "ERR:SERVICE_FAIL") && (code == 0);
+    }
 
-private:
-  std::string name_;
-  static size_t cpt;
-  static bool verbose_;
-  int error_code_;
-protected:
-  ontologenius::compat::onto_ros::Client<ontologenius::compat::OntologeniusIndexService> client_;
-};
+  private:
+    std::string name_;
+    static size_t cpt;
+    static bool verbose_;
+    int error_code_;
+
+  protected:
+    ontologenius::compat::onto_ros::Client<ontologenius::compat::OntologeniusIndexService> client_;
+  };
 
 } // namespace onto
 
