7b1513aa7ab9a7254b665c468c9900a573d0c43f
BastienDssrd <bastien.dussard@gmail.com>
2024-10-02 19:07:46 +0200

    [RuleGraph] updating the add function to create the Atoms of the rule

--- src/core/ontoGraphs/Graphs/RuleGraph.cpp
+++ src/core/ontoGraphs/Graphs/RuleGraph.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <iostream>
 
+#include "ontologenius/core/ontoGraphs/Graphs/AnonymousClassGraph.h"
 #include "ontologenius/core/ontoGraphs/Graphs/ClassGraph.h"
 #include "ontologenius/core/ontoGraphs/Graphs/DataPropertyGraph.h"
 #include "ontologenius/core/ontoGraphs/Graphs/IndividualGraph.h"
@@ -56,89 +57,102 @@ namespace ontologenius {
 
     for(auto& elem_antec : rule.antecedents)
     { // if atom is a class expression, create an unamed anonymous class
-      // if(elem_antec.first->is_complex)
-      std::vector<std::string> vect_atom = elem_antec.first->rest.toVector();
-
-      if(vect_atom.size() == 1)
+      if(elem_antec.first != nullptr)
       {
-        if(!elem_antec.first->rest.restriction_range.empty()) // single class atom
+        if(elem_antec.first->logical_type_ != logical_none || elem_antec.first->oneof == true || elem_antec.first->is_complex == true)
         {
-          ClassAtom* class_atom;
-          class_atom->class_expression->class_involved_ = class_graph_->findOrCreateBranch(elem_antec.first->rest.restriction_range);
+          std::cout << "complex atom " << std::endl;
+          ClassAtom* class_atom = new ClassAtom();
+          rule_antecedents->class_atoms_.push_back(class_atom);
+          size_t depth = 0;
+
+          AnonymousClassElement* class_elem = anonymous_graph_->createTree(elem_antec.first, depth);
+          class_atom->class_expression = class_elem;
+        }
+        else if(!elem_antec.first->rest.property.empty() && (!elem_antec.first->rest.restriction_range.empty() || !elem_antec.first->rest.card.cardinality_range.empty()))
+        {
+          std::cout << "single restriction " << std::endl;
+          ClassAtom* class_atom = new ClassAtom();
           rule_antecedents->class_atoms_.push_back(class_atom);
 
-          if(elem_antec.second.size() == 1)
-          {
-            std::cout << elem_antec.second.front() << std::endl;
-            if(isIn("indiv#", elem_antec.second.front()))
-              class_atom->individual_involved = individual_graph_->findOrCreateBranch(getName(elem_antec.second.front()));
-            else if(isIn("var#", elem_antec.second.front()))
-              class_atom->var = getName(elem_antec.second.front());
-            else
-              return nullptr;
-          }
+          AnonymousClassElement* class_elem = anonymous_graph_->createElement(elem_antec.first);
+          class_atom->class_expression = class_elem;
         }
-        else if(elem_antec.first->is_data_property) // data property atom
+        else // if the atom is of size 1, then it is either a class only atom, object or data property atom
         {
-          DataPropertyAtom* data_atom;
-          data_atom->data_property_expression = data_property_graph_->findOrCreateBranch(elem_antec.first->rest.property);
-          rule_antecedents->data_atoms_.push_back(data_atom);
-
-          if(elem_antec.second.size() == 2)
+          std::cout << "single atom " << std::endl;
+          if(!elem_antec.first->rest.restriction_range.empty()) // single class atom
           {
-            // get argument 1
-            std::string elem1 = elem_antec.second.front();
-            if(isIn("indiv#", elem1))
-              data_atom->individual_involved = individual_graph_->findOrCreateBranch(getName(elem1));
-            else if(isIn("var#", elem1))
-              data_atom->var1 = getName(elem1);
-            else
-              return nullptr;
+            std::cout << "single class atom with rest : " << elem_antec.first->rest.toString() << std::endl;
+            ClassAtom* class_atom = new ClassAtom();
+            AnonymousClassElement* class_elem = new AnonymousClassElement();
+            class_elem->class_involved_ = class_graph_->findOrCreateBranch(elem_antec.first->rest.restriction_range);
+            class_atom->class_expression = class_elem;
+            rule_antecedents->class_atoms_.push_back(class_atom);
 
-            // get argument 2
-            std::string elem2 = elem_antec.second.back();
-            if(isIn("var#", elem2))
-              data_atom->var2 = getName(elem2);
-            else
+            std::vector<Variable_t> current_var = elem_antec.second;
+
+            if(elem_antec.second.size() == 1)
             {
-              std::string type_datarange = elem2.substr(0, elem2.find("#") - 1);
-              data_atom->datatype_involved = data_property_graph_->createLiteral(type_datarange + "#");
+              Variable_t current_var;
+              if(current_var.is_instantiated)
+                class_atom->individual_involved = individual_graph_->findOrCreateBranch(current_var.var_name);
+              else
+                class_atom->var = current_var.var_name;
+            }
+          }
+          else if(elem_antec.first->is_data_property) // data property atom
+          {
+            std::cout << "single data atom " << elem_antec.first->rest.toString() << std::endl;
+            DataPropertyAtom* data_atom = new DataPropertyAtom();
+            data_atom->data_property_expression = data_property_graph_->findOrCreateBranch(elem_antec.first->rest.property);
+            rule_antecedents->data_atoms_.push_back(data_atom);
+
+            if(elem_antec.second.size() == 2)
+            {
+              // get argument 1
+              Variable_t var1 = elem_antec.second.front();
+              if(var1.is_instantiated)
+                data_atom->individual_involved = individual_graph_->findOrCreateBranch(var1.var_name);
+              else
+                data_atom->var1 = var1.var_name;
+
+              // get argument 2
+              Variable_t var2 = elem_antec.second.back();
+              if(var2.is_datavalue)
+              {
+                std::string type_datarange = var2.var_name.substr(0, var2.var_name.find("#") - 1);
+                data_atom->datatype_involved = data_property_graph_->createLiteral(type_datarange + "#");
+              }
+              else
+                data_atom->var2 = var2.var_name;
+            }
+          }
+          else // object property atom
+          {
+            std::cout << "single object atom " << elem_antec.first->rest.toString() << std::endl;
+            ObjectPropertyAtom* object_atom = new ObjectPropertyAtom();
+            object_atom->object_property_expression = object_property_graph_->findOrCreateBranch(elem_antec.first->rest.property);
+            rule_antecedents->object_atoms_.push_back(object_atom);
+
+            if(elem_antec.second.size() == 2)
+            {
+              // get argument 1
+              Variable_t var1 = elem_antec.second.front();
+              if(var1.is_instantiated)
+                object_atom->individual_involved_1 = individual_graph_->findOrCreateBranch(var1.var_name);
+              else
+                object_atom->var1 = var1.var_name;
+
+              // get argument 2
+              Variable_t var2 = elem_antec.second.back();
+              if(var2.is_instantiated)
+                object_atom->individual_involved_2 = individual_graph_->findOrCreateBranch(var2.var_name);
+              else
+                object_atom->var2 = var2.var_name;
             }
           }
         }
-        else // object property atom
-        {
-          ObjectPropertyAtom* object_atom;
-          object_atom->object_property_expression = object_property_graph_->findOrCreateBranch(elem_antec.first->rest.property);
-          rule_antecedents->object_atoms_.push_back(object_atom);
-
-          if(elem_antec.second.size() == 2)
-          {
-            // get argument 1
-            std::string elem1 = elem_antec.second.front();
-            if(isIn("indiv#", elem1))
-              object_atom->individual_involved_1 = individual_graph_->findOrCreateBranch(getName(elem1));
-            else if(isIn("var#", elem1))
-              object_atom->var1 = getName(elem1);
-            else
-              return nullptr;
-
-            // get argument 2
-            std::string elem2 = elem_antec.second.back();
-            if(isIn("indiv#", elem2))
-              object_atom->individual_involved_2 = individual_graph_->findOrCreateBranch(getName(elem2));
-            else if(isIn("var#", elem2))
-              object_atom->var2 = getName(elem2);
-            else
-              return nullptr;
-          }
-        }
-      }
-      else // complex class atom
-      {
-        ClassAtom* class_atom;
-        rule_antecedents->class_atoms_.push_back(class_atom);
-        // class_atom->class_expression = anonymous_graph_->createTree(ano.equivalence_trees[i], depth);
       }
     }
 
@@ -146,42 +160,7 @@ namespace ontologenius {
     // for(auto& elem_conseq : rule.consequents)
     // {
     // }
-
-    //     ClassBranch* class_branch = class_graph_->findOrCreateBranch(value);
-
-    //     anonymous_branch->class_equiv_ = class_branch;
-    //     all_branchs_.push_back(anonymous_branch);
-    //     class_branch->equiv_relations_ = anonymous_branch;
-
-    //     for(size_t i = 0; i < ano.equivalence_trees.size(); i++)
-    //     {
-    //       size_t depth = 0;
-    //       AnonymousClassElement* ano_elem = createTree(ano.equivalence_trees[i], depth);
-    //       ano_elem->ano_name = ano_name + "_" + std::to_string(i);
-    //       anonymous_branch->ano_elems_.push_back(ano_elem);
-    //       anonymous_branch->depth_ = depth;
-
-    // #ifdef DEBUG
-    //       printTree(ano_elem, 3, true);
-    // #endif
-    //     }
-
     return rule_branch;
   }
-
-  // void RuleGraph::createVariable(std::string variable)
-  // {
-  //   std::size_t pos = variable.find('#');
-  //   if(pos != std::string::npos)
-  //   {
-  //     std::string type_var = variable.substr(0, pos);
-  //     if(type_var == "indiv")
-  //       res += variable.substr(pos + 1);
-  //     else if(type_var == "var")
-  //       res += variable.substr(pos + 1);
-  //     else
-  //       res += variable.substr(pos + 1);
-  //   }
-  // }
 
 } // namespace ontologenius
