f6e36c4e4f04bb1acfbd9b06af2a0b8b96dba1ff
BastienDssrd <bastien.dussard@gmail.com>
2024-10-04 10:22:56 +0200

    [RuleChecker] adding the checker file for checking the rules

---
+++ include/ontologenius/core/ontoGraphs/Checkers/RuleChecker.h
@@ -0,0 +1,12 @@
+#ifndef ONTOLOGENIUS_RULECHECKER_H
+#define ONTOLOGENIUS_RULECHECKER_H
+
+#include "ontologenius/core/ontoGraphs/Checkers/ValidityChecker.h"
+#include "ontologenius/core/ontoGraphs/Graphs/RuleGraph.h"
+
+
+namespace ontologenius {
+
+} // namespace ontologenius
+
+#endif // ONTOLOGENIUS_RULECHECKER_H
---
+++ src/core/ontoGraphs/Checkers/RuleChecker.cpp
@@ -0,0 +1,10 @@
+#include "ontologenius/core/ontoGraphs/Checkers/RuleChecker.h"
+
+#include "ontologenius/core/ontoGraphs/Graphs/ClassGraph.h"
+#include "ontologenius/core/ontoGraphs/Graphs/DataPropertyGraph.h"
+#include "ontologenius/core/ontoGraphs/Graphs/ObjectPropertyGraph.h"
+#include "ontologenius/core/ontoGraphs/Graphs/IndividualGraph.h"
+
+namespace ontologenius {
+
+}  // namespace ontologenius
