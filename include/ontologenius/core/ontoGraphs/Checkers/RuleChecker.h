3a882eb87def5380a5a32e904a511cf7076025af
BastienDssrd <bastien.dussard@gmail.com>
2024-10-09 17:32:51 +0200

    [RuleChecker] adding the header for RuleChecker

--- include/ontologenius/core/ontoGraphs/Checkers/RuleChecker.h
+++ include/ontologenius/core/ontoGraphs/Checkers/RuleChecker.h
@@ -7,6 +7,40 @@
 
 namespace ontologenius {
 
+  class RuleChecker : public ValidityChecker<RuleBranch>
+  {
+  public:
+    explicit RuleChecker(RuleGraph* graph) : ValidityChecker(graph) { rule_graph_ = graph; }
+    ~RuleChecker() {}
+
+    size_t check() override;
+    void checkDisjoint();
+
+    void printStatus()
+    {
+      ValidityChecker<RuleBranch>::printStatus("rule", "rules", graph_vect_.size());
+    };
+
+  private:
+    RuleGraph* rule_graph_;
+
+    void checkAtomList(RuleAtomList* atom_list, std::unordered_map<std::string, std::vector<std::vector<ClassElement>>>& mapping_var_classes, std::set<std::string>& keys_variables);
+    void checkVariableMappings(std::unordered_map<std::string, std::vector<std::vector<ClassElement>>>& mapping_var_classes, std::set<std::string>& keys_variables);
+
+    std::vector<std::string> resolveInstantiatedClassAtom(ClassBranch* class_branch, IndividualBranch* indiv);
+    std::vector<std::string> resolveInstantiatedObjectPropertyAtom(ObjectPropertyBranch* property_branch, IndividualBranch* indiv_from, IndividualBranch* indiv_on);
+    std::vector<std::string> resolveInstantiatedDataPropertyAtom(DataPropertyBranch* data_property, IndividualBranch* indiv_from);
+
+    void resolveClassAtom(ClassAtom* class_atom, std::unordered_map<std::string, std::vector<std::vector<ClassElement>>>& mapping_var_classes, std::set<std::string>& keys_variables);
+    void resolveObjectAtom(ObjectPropertyAtom* object_atom, std::unordered_map<std::string, std::vector<std::vector<ClassElement>>>& mapping_var_classes, std::set<std::string>& keys_variables);
+    void resolveDataAtom(DataPropertyAtom* data_atom, std::unordered_map<std::string, std::vector<std::vector<ClassElement>>>& mapping_var_classes, std::set<std::string>& keys_variables);
+    // void resolveBuiltinAtom(ClassAtom* class_atom, std::unordered_map<std::string, std::vector<std::vector<ClassElement>>>& mapping_var_classes);
+
+    void getUpperLevelDomains(AnonymousClassElement* class_expression, std::vector<ClassElement>& expression_domains);
+    std::string checkClassesDisjointness(ClassBranch* class_left, ClassBranch* class_right);
+    std::vector<std::string> checkClassesVectorDisjointness(const std::vector<ClassElement>& classes_left, const std::vector<ClassElement>& class_right);
+  };
+
 } // namespace ontologenius
 
 #endif // ONTOLOGENIUS_RULECHECKER_H
