5ea2a9bd3ba2e31c89f303536b8556acc106b12d
BastienDssrd <bastien.dussard@gmail.com>
2024-10-31 08:08:54 +0100

    [AnonymousClassGraph]  adding the function to create the hidden class for rule usage

--- src/core/ontoGraphs/Graphs/AnonymousClassGraph.cpp
+++ src/core/ontoGraphs/Graphs/AnonymousClassGraph.cpp
@@ -47,11 +47,16 @@ namespace ontologenius {
     }
   }
 
-  AnonymousClassElement* AnonymousClassGraph::createElement(ExpressionMember_t* exp)
+  AnonymousClassElement* AnonymousClassGraph::createElement(ExpressionMember_t* exp, AnonymousClassElement* root_node)
   {
     AnonymousClassElement* ano_element = new AnonymousClassElement();
     ano_element->is_complex = exp->is_complex;
     Restriction_t current_rest = exp->rest;
+
+    if(root_node != nullptr)
+      ano_element->root_node_ = root_node;
+    else
+      ano_element->root_node_ = ano_element;
 
     // ============= Node type =================
     if(exp->logical_type_ != logical_none)
@@ -69,9 +74,15 @@ namespace ontologenius {
     if(!current_rest.property.empty())
     {
       if(exp->is_data_property)
+      {
         ano_element->data_property_involved_ = data_property_graph_->findOrCreateBranch(current_rest.property);
+        ano_element->root_node_->involves_data_property = true;
+      }
       else
+      {
         ano_element->object_property_involved_ = object_property_graph_->findOrCreateBranch(current_rest.property);
+        ano_element->root_node_->involves_object_property = true;
+      }
     }
 
     // ==============  Cardinality Type & Number =============
@@ -105,13 +116,20 @@ namespace ontologenius {
       {
         const std::string type_value = card_range.substr(card_range.find("#") + 1, -1);
         ano_element->card_.card_range_ = data_property_graph_->createLiteral(type_value);
+        ano_element->root_node_->involves_data_property = true;
       }
       else // object property
       {
         if(ano_element->card_.card_type_ == cardinality_value) // indiv
+        {
           ano_element->individual_involved_ = individual_graph_->findOrCreateBranch(card_range);
-        else // class
+          ano_element->root_node_->involves_individual = true;
+        }
+        else
+        { // class
           ano_element->class_involved_ = class_graph_->findOrCreateBranch(card_range);
+          ano_element->root_node_->involves_class = true;
+        }
       }
     }
 
@@ -125,23 +143,33 @@ namespace ontologenius {
         ano_element->card_.card_range_ = data_property_graph_->createLiteral(type);
       }
       else if(exp->mother != nullptr && exp->mother->oneof) // individual node for oneOf (ClassX Eq to oneOf(indiv))
+      {
         ano_element->individual_involved_ = individual_graph_->findOrCreateBranch(rest_range);
+        ano_element->root_node_->involves_individual = true;
+      }
       else
+      {
         ano_element->class_involved_ = class_graph_->findOrCreateBranch(rest_range); // class node for class only restriction (ClassX Eq to ClassY)
+        ano_element->root_node_->involves_class = true;
+      }
     }
 
     return ano_element;
   }
-  
-  AnonymousClassElement* AnonymousClassGraph::createTree(ExpressionMember_t* member_node, size_t& depth)
+
+  AnonymousClassElement* AnonymousClassGraph::createTree(ExpressionMember_t* member_node, size_t& depth, AnonymousClassElement* root_node)
   {
     size_t local_depth = depth + 1;
-    AnonymousClassElement* node = createElement(member_node);
+
+    AnonymousClassElement* node = createElement(member_node, root_node);
+
+    if(root_node == nullptr)
+      root_node = node;
 
     for(auto* child : member_node->child_members)
     {
       size_t child_depth = depth + 1;
-      node->sub_elements_.push_back(createTree(child, child_depth));
+      node->sub_elements_.push_back(createTree(child, child_depth, root_node));
       if(child_depth > local_depth)
         local_depth = child_depth;
     }
@@ -171,11 +199,40 @@ namespace ontologenius {
       anonymous_branch->ano_elems_.push_back(ano_elem);
       anonymous_branch->depth_ = depth;
 
+      // std::cout << " c : " << ano_elem->root_node_->involves_class << " o : " << ano_elem->root_node_->involves_object_property
+      //           << " d : " << ano_elem->root_node_->involves_data_property << " i : " << ano_elem->root_node_->involves_individual << std::endl;
+
 #ifdef DEBUG
       printTree(ano_elem, 3, true);
 #endif
     }
 
+    return anonymous_branch;
+  }
+
+  AnonymousClassBranch* AnonymousClassGraph::addHiddenRuleElem(const size_t& rule_id, const size_t& elem_id, ExpressionMember_t* ano_expression)
+  {
+    const std::string current_elem = std::to_string(rule_id) + "_" + std::to_string(elem_id);
+    const std::string ano_name = "anonymous_rule_" + current_elem;
+    const std::string hidden_class_name = "__rule_" + current_elem;
+
+    AnonymousClassBranch* anonymous_branch = new AnonymousClassBranch(ano_name);
+
+    ClassBranch* hidden_class_branch = class_graph_->findOrCreateBranch(hidden_class_name, true); // this ClassBranch has to be hidden
+    hidden_class_branch->equiv_relations_ = anonymous_branch;
+    anonymous_branch->class_equiv_ = hidden_class_branch;
+    all_branchs_.push_back(anonymous_branch);
+
+    size_t depth = 0;
+    AnonymousClassElement* ano_elem = createTree(ano_expression, depth);
+    ano_elem->ano_name = ano_name;
+    anonymous_branch->ano_elems_.push_back(ano_elem);
+    anonymous_branch->depth_ = depth;
+
+    // std::cout << " c : " << ano_elem->root_node_->involves_class << " o : " << ano_elem->root_node_->involves_object_property
+    //           << " d : " << ano_elem->root_node_->involves_data_property << " i : " << ano_elem->root_node_->involves_individual << std::endl;
+
+    // std::cout << "created ano : " << anonymous_branch->value() << " and hidden class : " << hidden_class_branch->value() << std::endl;
     return anonymous_branch;
   }
 
