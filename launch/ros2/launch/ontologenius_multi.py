9fc3f31f17d0392539feceeabc5c09aaf85df10c
Guillaume Sarthou <gsarthou@laas.fr>
2025-07-02 19:28:19 +0200

    [launch] add ontologenius_milti launch files

---
+++ launch/ros2/launch/ontologenius_multi.py
@@ -0,0 +1,76 @@
+import os
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.actions import GroupAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import TextSubstitution
+from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace
+
+
+def generate_launch_description():
+
+    # args that can be set from the command line or a default will be used
+    language_arg = DeclareLaunchArgument(
+        "language", default_value=TextSubstitution(text="en")
+    )
+    intern_file_arg = DeclareLaunchArgument(
+        "intern_file", default_value=TextSubstitution(text=os.path.join(get_package_share_directory('ontologenius'), "file_intern/ontologenius.owl"))
+    )
+    config_file_arg = DeclareLaunchArgument(
+        "config_file", default_value=TextSubstitution(text="none")
+    )
+    display_arg = DeclareLaunchArgument(
+        "display", default_value=TextSubstitution(text="true")
+    )
+    root_arg = DeclareLaunchArgument(
+        "root", default_value=TextSubstitution(text="none")
+    )
+    files_arg = DeclareLaunchArgument(
+        "files", default_value=TextSubstitution(text = os.path.join(get_package_share_directory('ontologenius'), "files/attribute.owl") + " " +
+                                                       os.path.join(get_package_share_directory('ontologenius'), "files/animals.owl") + " " +
+                                                       os.path.join(get_package_share_directory('ontologenius'), "files/positionProperty.owl") + " " +
+                                                       os.path.join(get_package_share_directory('ontologenius'), "files/test_individuals.owl"))
+    )
+    robot_file_arg = DeclareLaunchArgument(
+        "robot_file", default_value=TextSubstitution(text="none")
+    )
+    human_file_arg = DeclareLaunchArgument(
+        "human_file", default_value=TextSubstitution(text="none")
+    )
+    tcmalloc_path_arg = DeclareLaunchArgument(
+        "tcmalloc_path", default_value=TextSubstitution(text="")
+    )
+
+    ontologenius_core_node = Node(
+            package='ontologenius',
+            executable='ontologenius_multi',
+            name='ontologenius_core',
+            output='screen',
+            arguments=['--root', LaunchConfiguration('root'),
+                       '-l', LaunchConfiguration('language'),
+                       '-c', LaunchConfiguration('config_file'),
+                       '-i', LaunchConfiguration('intern_file'),
+                       '-r', LaunchConfiguration('robot_file'),
+                       '-h', LaunchConfiguration('human_file'),
+                       '-d', LaunchConfiguration('display'),
+                       LaunchConfiguration('files')]
+        )
+
+    return LaunchDescription([
+        root_arg,
+        language_arg,
+        intern_file_arg,
+        config_file_arg,
+        display_arg,
+        files_arg,
+        robot_file_arg,
+        human_file_arg,
+        tcmalloc_path_arg,
+        ontologenius_core_node
+    ])
